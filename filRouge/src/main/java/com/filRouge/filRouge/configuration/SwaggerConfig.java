2cc2d219222f4e4c40563e0fc57fd16bf38a7464
timotheehrl <tharleaux@gmail.com>
2022-09-01 11:48:37 +0200

    fix swagger ui

--- filRouge/src/main/java/com/filRouge/filRouge/configuration/SwaggerConfig.java
+++ filRouge/src/main/java/com/filRouge/filRouge/configuration/SwaggerConfig.java
@@ -23,39 +23,13 @@ public class SwaggerConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
+    .select()
+.apis(RequestHandlerSelectors.basePackage("com.filRouge.filRouge.controller"))
+//            .paths(PathSelectors.regex("/Produits.*"))
             .build();
-
   }
 
-  private ApiInfo metadata() {
-    return new ApiInfoBuilder()//
-        .title("JSON Web Token Authentication API")//
-        .description("This is a sample JWT authentication service. You can find out more about JWT at [https://jwt.io/](https://jwt.io/). For this sample, you can use the `admin` or `client` users (password: admin and client respectively) to test the authorization filters. Once you have successfully logged in and obtained the token, you should click on the right top button `Authorize` and introduce it with the prefix \"Bearer \".")//
-        .version("1.0.0")//
-        .license("MIT License").licenseUrl("http://opensource.org/licenses/MIT")//
-        .contact(new Contact(null, null, "admin.crm@gmail.com"))//
-        .build();
-  }
   
-  private ApiKey apiKey() {
-    return new ApiKey("Authorization", "Authorization", "header");
-  }
+     }
 
-  private SecurityContext securityContext() {
-    return SecurityContext.builder()
-        .securityReferences(defaultAuth())
-        .forPaths(PathSelectors.any())
-        .build();
-  }
 
-  private List<SecurityReference> defaultAuth() {
-    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-    authorizationScopes[0] = authorizationScope;
-    return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
-  }
-
-}
